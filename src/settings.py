4ce0fe42abaf2fb098ac6c72609b3773ce2c9b0d
aaamab257 <46580571+aaamab257@users.noreply.github.com>
2022-11-04 15:34:14 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0=un+5h4^wqq*6#i_dydto$8uiwbd1@vl$c=#5o8p(o8*)rr=e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
